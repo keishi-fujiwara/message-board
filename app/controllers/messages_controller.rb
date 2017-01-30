8059c856b75e30015900703ee452bd0f8d526caa
keishifujiwara <keishi0309@gmail.com>
2017-01-30 13:48:34 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     @messages = Message.all
+  end
+  
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
 
   def create
@@ -15,6 +29,11 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+   def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
   def message_params
@@ -22,3 +41,7 @@ class MessagesController < ApplicationController
   end
   ## ここまで
 end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
